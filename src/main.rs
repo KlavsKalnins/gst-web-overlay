20a08e5ac4888ce8c0372a42bbf41b7bb39cc0f5
klavs kalnins <klavskalninss@gmail.com>
2021-09-16 14:09:03 +0300

    might have forgotten this

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() {
         Ok(_) => (),
         Err(e) => println!("Error creating dir: {}", e.to_string()),
     }
-    
+
     thread::spawn(|| {
         let base_args: String = "v4l2src device=/dev/video0 \
             ! videoconvert ! videoscale ! video/x-raw,width=640,height=480 \
